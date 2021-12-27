c12031489cad32139cb991fcd22ed744a1bf5025
NdelogakehDaniel <ndabosed@gmail.com>
2021-12-27 13:28:19 +0100

    django todolist app

--- todolist/settings.py
+++ todolist/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
             'NAME': 'djtodolist',  
             'USER':'root',  
             'PASSWORD':'',  
-            'HOST':'localhost',  
+            'HOST':'127.0.0.1',  
             'PORT':'3306'  
         }  
     }  
